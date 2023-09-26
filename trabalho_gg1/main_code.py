5ff4f140a2c1b9d4865bfea26ef33d7206cde89e
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2023-09-26 18:48:02 -0300

    Update main_code.py

--- trabalho_gg1/main_code.py
+++ trabalho_gg1/main_code.py
@@ -15,34 +15,34 @@ def main():
     while True:
         option = menu_module.show_menu()
 
-        if option == 1:
+        if option == "1":
             stock_module.add_product(stock_data, add_alter_record)
 
-        elif option == 2:
+        elif option == "2":
             stock_module.alter_price(stock_data, value_alter_record)
             
-        elif option == 3:
+        elif option == "3":
             stock_module.remove_product(stock_data, removal_record)
             
-        elif option == 4:
+        elif option == "4":
             historic_module.alter_report(add_alter_record, value_alter_record, removal_record)
             
-        elif option == 5:
+        elif option == "5":
             stock_module.search_for_product(stock_data, sales_record) 
 
-        elif option == 6:
+        elif option == "6":
             stock_module.search_product_by_category(stock_data)
 
-        elif option == 7:
+        elif option == "7":
             stock_module.show_products(stock_data)
     
-        elif option == 8:
+        elif option == "8":
             sales_module.sell_product(stock_data, sales_record)
         
-        elif option == 9:
+        elif option == "9":
             historic_module.sales_report(sales_record)
           
-        elif option == 10:
+        elif option == "10":
             print("Obrigada por usar nossos serviços!")
             break
         
