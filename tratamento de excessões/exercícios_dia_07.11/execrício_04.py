60ef991a05bfd4ecca705e4103b2a11a463ffffb
mell <126731231+mellcs@users.noreply.github.com>
2023-11-07 22:27:48 -0300

    Create execrício_04.py

---
+++ tratamento de excessões/exercícios_dia_07.11/execrício_04.py
@@ -0,0 +1,12 @@
+# Crie uma função chamada find_element que aceita uma lista e um índice como entrada e retorna o elemento na posição do índice fornecido. Se o índice estiver fora
+# dos limites da lista, lance uma exceção IndexError com uma mensagem indicando que o índice é inválido.
+
+def find_element(my_list, ):
+    
+    
+    
+def main():
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    index = int(input("Insira o índice desejado: "))
+
+main()
